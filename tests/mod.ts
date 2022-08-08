f6294d23d176974a21f60d51c9578b9703f27804
Taras <tarasikee@users.noreply.github.com>
2022-08-08 16:31:48 +0200

    Test steps update

--- tests/mod.ts
+++ tests/mod.ts
@@ -3,6 +3,7 @@ import {userDocument, userModel} from "./userInitiale.ts"
 import {faker, format} from "../deps/deps.ts"
 import TestContext = Deno.TestContext
 
+
 Deno.test("database", async (t: TestContext) => {
   await t.step("Create users using userModel, skipping types check with `as` keyword",
     () => {
