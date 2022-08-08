b2426e26a839111a3c50006598038f8e3f17aa03
Taras <tarasikee@users.noreply.github.com>
2022-08-08 16:21:48 +0200

    Test steps update

--- tests/mod.ts
+++ tests/mod.ts
@@ -1,45 +1,23 @@
-import {userModel, userDocument} from "./userInitiale.ts"
+import {userDocument, userModel} from "./userInitiale.ts"
 
 import {faker, format} from "../deps/deps.ts"
 
 
-const users = [...Array(500).keys()].map(() => userModel.create({
-  name: faker.name.firstName() + " " + faker.name.lastName(),
-  birthday: format(faker.date.past(), "yyyy-MM-dd"),
-  isAdmin: faker.random.boolean(),
-  settings: {
-    theme: faker.random.arrayElement(["light", "dark"]),
-    lang: faker.random.arrayElement(["en", "fr"])
-  }
-} as unknown as userDocument))
+Deno.test("database", async t => {
+  await t.step("Create users using userModel, skipping types check with `as` keyword",
+    () => {
+      const users = [...Array(200).keys()].map(() => userModel.create({
+        name: faker.name.firstName() + " " + faker.name.lastName(),
+        birthday: format(faker.date.past(), "yyyy-MM-dd"),
+        isAdmin: faker.random.boolean(),
+        settings: {
+          theme: faker.random.arrayElement(["light", "dark", "system"]),
+          lang: faker.random.arrayElement(["en", "fr", "no", "de", "ua"])
+        }
+      } as unknown as userDocument))
 
-users.map(user => user.save())
 
-// const candidates = userModel.find({
-//     settings: {
-//         theme: {
-//             night: {
-//                 second: {
-//                     background: {
-//                         r: 2,
-//                         b: 1
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// })
-// console.log(candidates)
-
-// const users = userModel.findAll()
-// users.map(user => {
-//     user.fields.settings = {
-//         theme: "darcula",
-//         lang: "en"
-//     }
-//
-//     user.fields.isAdmin = false
-//
-//     user.save()
-// })
-
+      users.map(user => user.save())
+    }
+  )
+})
